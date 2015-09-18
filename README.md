6b97804665432fedb9a32fe4845bc480f929e2c8
Russell Hay <me@russellhay.com>
2015-09-17 19:49:47 -0700

    Initial Checkin

---
+++ README.md
@@ -0,0 +1,2 @@
+Super simple library for storing and sharing the service UUIDs for the bluetooth
+device, the imuduino
---
+++ service-info.js
@@ -0,0 +1,5 @@
+module.exports = {
+   uart: '6e400001b5a3f393e0a9e50e24dcca9e',
+   rx: '6e400003b5a3f393e0a9e50e24dcca9e',
+   tx: '6e400002b5a3f393e0a9e50e24dcca9e'
+};
